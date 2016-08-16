41fedff002fc48eb7d478b9e72199492033042c3
suhyun Lee <ilyf28@gmail.com>
2016-08-16 17:14:52 +0900

    modify title

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,7 +2,7 @@
 <html class="no-js" lang="en">
 <head>
 <meta charset="utf-8" />
-<title>Stigma2::install</title>
+<title>Stigma2::management</title>
 
 <link rel="stylesheet" href="/css/app.css" />
 
