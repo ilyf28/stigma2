447d398bc5f64a4be5b4e1fe0fe95c5381bfdcc4
suhyun Lee <ilyf28@gmail.com>
2016-08-18 13:55:16 +0900

    delete Provision

--- pkg/Provision/src/Models/ProvisionedServer.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Stigma\Provision\Models ;
-
-class ProvisionedServer extends \Eloquent
-{
-    protected $table = 'provisioned_server' ; 
-
-    protected $guarded = array();
-}
--- pkg/Provision/src/Repositories/ProvisionedServerRepository.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Stigma\Provision\Repositories ;
-use Stigma\Database\Repository\IlluminateRepository ;
-
-class ProvisionedServerRepository extends IlluminateRepository
-{
-    protected $model = 'Stigma\Provision\Models\ProvisionedServer' ;
-}
