51dd101cabf08446fe7eeb58426fca657a0d119f
suhyun Lee <ilyf28@gmail.com>
2016-08-31 22:55:10 +0900

    delete temp files

--- resources/views/admin/dashboard/host.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="panel white-panel  radius">
-    <h5>MONITORING HOSTS</h5> 
-    <div class="row health-check-area">
-        
-    </div>
-</div>
-
--- resources/views/admin/dashboard/service.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="panel white-panel  radius">
-    <h5>MONITROING SERVICES</h5> 
-    <div class="row health-check-area">
-        
-    </div>
-</div>
-
