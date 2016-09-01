6a7b1c7ffb82c38d024b03660244ece929bf8822
suhyun Lee <ilyf28@gmail.com>
2016-09-01 16:12:23 +0900

    remove openshift config

--- config/database.php
+++ config/database.php
@@ -53,20 +53,6 @@ return [
         ],
 
         'mysql' => [
-                'driver' => 'mysql',
-                'host' => env('DB_HOST', env('OPENSHIFT_MYSQL_DB_HOST', 'localhost')),
-                'port' => env('DB_PORT', env('OPENSHIFT_MYSQL_DB_PORT', 3306)),
-                'database' => env('DB_DATABASE', env('OPENSHIFT_APP_NAME', 'forge')),
-                'username' => env('DB_USERNAME', env('OPENSHIFT_MYSQL_DB_USERNAME', 'forge')),
-                'password' => env('DB_PASSWORD', env('OPENSHIFT_MYSQL_DB_PASSWORD', '')),
-                'charset' => 'utf8',
-                'collation' => 'utf8_unicode_ci',
-                'prefix' => '',
-                'strict' => false,
-        ],
-
-        /*
-        'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
@@ -78,7 +64,6 @@ return [
             'prefix' => '',
             'strict' => false,
         ],
-        */
 
         'pgsql' => [
             'driver'   => 'pgsql',
