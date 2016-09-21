faaa2f5d0ba20c4a7dafa8f25e2a93f2464887c8
suhyun Lee <ilyf28@gmail.com>
2016-09-21 17:57:48 +0900

    add contactgroup list page

--- app/Http/Controllers/Admin/ContactGroupController.php
+++ app/Http/Controllers/Admin/ContactGroupController.php
@@ -4,81 +4,95 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+use Stigma\ObjectManager\ContactManager;
+use Stigma\ObjectManager\ContactgroupManager;
 
 class ContactgroupController extends Controller {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
+    protected $contactManager;
+    protected $contactgroupManager;
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
+    public function __construct(ContactManager $contactManager, ContactgroupManager $contactgroupManager)
+    {
+        $this->contactManager = $contactManager;
+        $this->contactgroupManager = $contactgroupManager;
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $items = $this->contactgroupManager->getAllItems();
+        return view('admin.contactgroup.index', compact('items'));
+    }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        //
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 
 }
---
+++ resources/views/admin/contactgroup/index.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.admin')
+
+@section('contents')
+<ul class="breadcrumbs">
+  <li><a href="#">Admin</a></li>
+  <li><a href="#">Contactgroup Manager</a></li>
+  <li class="current"><a href="#">List</a></li>
+</ul>
+
+<div class="inner-content">
+
+    <a href="{{route('admin.contactgroups.create')}}" class="button small right" >Create</a>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th width="50">#</th>
+                <th width="200">Contact Group Name</th>
+                <th>Alias</th>
+                <th width="50"></th>
+                <th width="50"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($items as $item)
+            <tr>
+                <td>{{$item->getKey()}}</td>
+                <td>{{$item->contactgroup_name}}</td>
+                <td>{{$item->alias}}</td>
+                <td style="text-align:center;"><a class="update-btn" href="{{route('admin.contactgroups.edit', array($item->getKey()))}}"><i class="fi-widget"></i></a></td>
+                <td style="text-align:center;"><a class="alert delete-btn" data-reveal-id="delete-modal" data-contactgroup-id="{{$item->getKey()}}"><i class="fi-trash"></i></a></td> 
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div> 
+
+<div id="delete-modal" class="reveal-modal small modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
+    <div class="modal-header">
+        <h5 class="title">Delete Contact Group</h5>
+        <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+    </div>
+    <div class="modal-body"> 
+        {!! Form::open(array('route'=> 'admin.contactgroups.index' ,'id'=> 'delete-form')) !!} 
+        <input type="hidden" name="_token" value="{{csrf_token()}}" />
+        <div data-alert class="stigma-alert-box alert"> 
+            <span class="fi-info"></span>&nbsp; Do you want to delete a contact group?
+        </div>
+        {!! Form::close() !!}
+    </div> 
+    <div class="modal-footer"> 
+        <a class="button right small alert request-to-delete"><span class="fi-trash"></span>&nbsp;Delete</a> 
+    </div>
+</div>
+
+
+@stop
+
+@section('scripts')
+<script> 
+jQuery(function($){
+    var id; 
+
+    $('.delete-btn').click(function(){
+        id = $(this).data('contactgroup-id');
+    });
+
+    $('.request-to-delete').click(function(){
+        var $form = $('#delete-form');
+        var data = $form.serialize();
+        var url = $form.attr('action');
+
+        $.ajax({ 
+            'type': 'delete', 
+            'url' : url+'/'+id, 
+            'data' : {
+                '_token' : $form.find('[name=_token]').val()
+            },
+            'success':function(){ 
+                location.href = location.href;
+            }
+        });
+    }); 
+});
+</script>
+@stop
