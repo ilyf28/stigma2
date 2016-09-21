f122a870f1fbfa81c293dfa057f21cc604d62a97
suhyun Lee <ilyf28@gmail.com>
2016-09-21 17:27:12 +0900

    add hostgroup list page

---
+++ resources/views/admin/hostgroup/index.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.admin')
+
+@section('contents')
+<ul class="breadcrumbs">
+  <li><a href="#">Admin</a></li>
+  <li><a href="#">Hostgroup Manager</a></li>
+  <li class="current"><a href="#">List</a></li>
+</ul>
+
+<div class="inner-content">
+
+    <a href="{{route('admin.hostgroups.create')}}" class="button small right" >Create</a>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th width="50">#</th>
+                <th width="200">Host Group Name</th>
+                <th>Alias</th>
+                <th width="50"></th>
+                <th width="50"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($items as $item)
+            <tr>
+                <td>{{$item->getKey()}}</td>
+                <td>{{$item->hostgroup_name}}</td>
+                <td>{{$item->alias}}</td>
+                <td style="text-align:center;"><a class="update-btn" href="{{route('admin.hostgroups.edit', array($item->getKey()))}}"><i class="fi-widget"></i></a></td>
+                <td style="text-align:center;"><a class="alert delete-btn" data-reveal-id="delete-modal" data-hostgroup-id="{{$item->getKey()}}"><i class="fi-trash"></i></a></td> 
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div> 
+
+<div id="delete-modal" class="reveal-modal small modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
+    <div class="modal-header">
+        <h5 class="title">Delete Host Group</h5>
+        <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+    </div>
+    <div class="modal-body"> 
+        {!! Form::open(array('route'=> 'admin.hostgroups.index' ,'id'=> 'delete-form')) !!} 
+        <input type="hidden" name="_token" value="{{csrf_token()}}" />
+        <div data-alert class="stigma-alert-box alert"> 
+            <span class="fi-info"></span>&nbsp; Do you want to delete a host group?
+        </div>
+        {!! Form::close() !!}
+    </div> 
+    <div class="modal-footer"> 
+        <a class="button right small alert request-to-delete"><span class="fi-trash"></span>&nbsp;Delete</a> 
+    </div>
+</div>
+
+
+@stop
+
+@section('scripts')
+<script> 
+jQuery(function($){
+    var id; 
+
+    $('.delete-btn').click(function(){
+        id = $(this).data('hostgroup-id');
+    });
+
+    $('.request-to-delete').click(function(){
+        var $form = $('#delete-form');
+        var data = $form.serialize();
+        var url = $form.attr('action');
+
+        $.ajax({ 
+            'type': 'delete', 
+            'url' : url+'/'+id, 
+            'data' : {
+                '_token' : $form.find('[name=_token]').val()
+            },
+            'success':function(){ 
+                location.href = location.href;
+            }
+        });
+    }); 
+});
+</script>
+@stop
