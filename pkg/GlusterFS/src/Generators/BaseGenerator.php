7499bd709d70654d02efeb801b721df6a84b19d6
suhyun Lee <ilyf28@gmail.com>
2016-10-03 21:53:29 +0900

    add unlink when file exist

--- pkg/GlusterFS/src/Generators/BaseGenerator.php
+++ pkg/GlusterFS/src/Generators/BaseGenerator.php
@@ -13,6 +13,10 @@ abstract class BaseGenerator
 
     public function write(array $conf)
     {
+        if (file_exists($this->outputPath)) {
+            unlink($this->outputPath);
+        }
+
         $result = file_put_contents($this->outputPath, implode("\n", $conf) . "\n", FILE_APPEND);
 
         return true;
