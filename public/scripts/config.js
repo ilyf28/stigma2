29b545e7c39c763c29c855f7e6e67e43537595d4
suhyun Lee <ilyf28@gmail.com>
2016-10-07 11:13:51 +0900

    add host graph

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -10,6 +10,7 @@ define([], function() {
             {'state': 'glusterfsList', 'url': '/glusterfs', 'templateUrl': PARTIALS_ROOT + 'glusterfs.index.php', 'controller': 'GlusterfsListCtrl'},
             {'state': 'serverHostList', 'url': CONTEXT_ROOT + '/hosts', 'templateUrl': PARTIALS_ROOT + 'server.host.index.php', 'controller': 'ServerHostListCtrl'},
             {'state': 'serverHostDetail', 'url': CONTEXT_ROOT + '/hosts/:name', 'templateUrl': PARTIALS_ROOT + 'server.host.show.php', 'controller': 'ServerHostShowCtrl'},
+            {'state': 'serverHostGraphDetail', 'url': CONTEXT_ROOT + '/hosts/graph/:name', 'templateUrl': PARTIALS_ROOT + 'report.graph.php', 'controller': 'ServerHostGraphCtrl'},
             {'state': 'serverServiceList', 'url': CONTEXT_ROOT + '/services', 'templateUrl': PARTIALS_ROOT + 'server.service.index.php', 'controller': 'ServerServiceListCtrl'},
             {'state': 'serverServiceDetail', 'url': CONTEXT_ROOT + '/services/:name/:servicedescription', 'templateUrl': PARTIALS_ROOT + 'server.service.show.php', 'controller': 'ServerServiceShowCtrl'},
             {'state': 'reportGraph', 'url': CONTEXT_ROOT + '/report/graph', 'templateUrl': PARTIALS_ROOT + 'report.graph.php', 'controller': 'ReportGraphCtrl'},
