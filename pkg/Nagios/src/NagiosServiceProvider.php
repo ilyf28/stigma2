fb0fd1bf26ab1dd887fcbee39c27766a7ad77046
suhyun <sh@suhyuns-MacBook-Pro.local>
2016-11-09 11:16:30 +0900

    delete api port

--- pkg/Nagios/src/NagiosServiceProvider.php
+++ pkg/Nagios/src/NagiosServiceProvider.php
@@ -12,7 +12,7 @@ class NagiosServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind('Stigma\Nagios\Client' ,function() {
-            $baseUri = 'http://'.config('nagios.host').':'.config('nagios.apiport');
+            $baseUri = 'http://'.config('nagios.host');
             $baseUri = rtrim($baseUri, '/');
             $baseUri = $baseUri.'/';
             return new \Stigma\Nagios\Client($baseUri);
